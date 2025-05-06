12628f8bb195a9b950603d37f7a1a39e849610f7
nabiha <150027037+nbhznb@users.noreply.github.com>
2025-05-06 18:20:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,9 @@ def create_app():
 
     return app
 
+# Create a global app variable for gunicorn to use
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
